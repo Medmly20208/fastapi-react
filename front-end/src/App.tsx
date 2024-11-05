9cee3af9facf5d0960c2fd7c44b47e9c1ad0df59
Mohamed Moulay <moulaymohamed856@gmail.com>
2024-11-05 16:38:44 +0100

    add prod links

--- front-end/src/App.tsx
+++ front-end/src/App.tsx
@@ -53,7 +53,7 @@ function App() {
     };
 
     try {
-      const response = await fetch("http://127.0.0.1:8000/predict", {
+      const response = await fetch("https://fastapi-react-c560.onrender.com", {
         method: "POST",
         headers: {
           "Content-Type": "application/json",
